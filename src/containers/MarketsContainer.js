e0857deeaa236055f57faf7fc3ad65f3fd003845
filmscore88 <cmtmorales24@gmail.com>
2019-08-09 10:38:49 -0500

    remove special characters using regex on market card

--- src/containers/MarketsContainer.js
+++ src/containers/MarketsContainer.js
@@ -22,18 +22,21 @@ class MarketsContainer extends Component{
     }
     return(
       <div>
-        <ul>
+          <h1>Farmers Markets</h1>
           {this.props.markets.map(market => (
+
+
+
             <div className= "MarketCard">
-              <p>Name: {market.name} </p>
-              <p>Address: {market.address} </p>
-              <a href= {`${market.GoogleLink}`}> GoogleMaps </a>
-              <p>Products: {market.products} </p>
-              <p>Schedule: {market.schedule} </p>
+              <h3>{market.name.replace(/\d*/g,'')}</h3>
               <img src= "https://s3-media3.fl.yelpcdn.com/bphoto/PTd0DMmirVaUICMvCObj3w/258s.jpg"/>
+              <p>Address: {market.address} </p>
+              <p>Products: {market.products} </p>
+              <p>Schedule: {market.schedule.replace(/\<br\>/g," ")} </p>
+              <a href= {`${market.GoogleLink}`}> GoogleMaps </a>
             </div>
-            ))}
-        </ul>
+          ))}
+
       </div>
     )
   }
