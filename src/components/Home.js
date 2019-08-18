81568e7664cd58167bde2815b5da86a1289b711e
filmscore88 <cmtmorales24@gmail.com>
2019-08-17 20:33:48 -0500

    added styling css for homepage and navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,9 @@ export default class Home extends Component {
     render(){
 
       return(
-        <div>
-          <h1>Welcome To Farmers Markets</h1>
-          <p> image....</p>
+        <div className="Homepage">
+          <h1>Welcome</h1>
+          <img src= "https://www.shelmerdine.com/wp-content/uploads/2018/05/Farmers-Market.jpg" />
         </div>
 
       )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,13 +7,18 @@ import { NavLink } from 'react-router-dom'
 
 
    return(
-     <div className= "NavBar">
-     <NavLink to='/'>Home</NavLink>
-     <NavLink to='/markets'>Markets</NavLink>
+     <div style={{background: '#e9e3d4', borderBottom: '2px solid black', paddingTop: '10px', paddingBottom: '10px', marginBottom: '12px', textAlign: 'center'}}>
+     <NavLink style={{marginRight : '40px'}} to='/'>
+       Home
+       <span className="glyphicon glyphicon-home"></span>
+     </NavLink>
+     <NavLink style={{marginRight : '40px'}} to='/markets'>
+        Markets
+        <span className="glyphicon glyphicon glyphicon-sunglasses"></span>
+     </NavLink>
      </div>
    )
 
  }
 
  export default NavBar;
- 
