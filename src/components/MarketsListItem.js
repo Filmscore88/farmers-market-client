8d5f1da190ab555ee3d0e21f32a1fb58fa62fcb0
filmscore88 <cmtmorales24@gmail.com>
2019-08-18 10:54:38 -0500

    added pictures to each route

--- src/components/MarketsListItem.js
+++ src/components/MarketsListItem.js
@@ -11,12 +11,12 @@ class MarketsListItem extends Component {
     return(
       this.props.market.map( market => (
         <div key= {market.id} className= "MarketCard">
-          <h3>{market.name.replace(/\d*/g,'')}</h3>
-          <img src= "https://s3-media3.fl.yelpcdn.com/bphoto/PTd0DMmirVaUICMvCObj3w/258s.jpg"/>
-          <p>Address: {market.address} </p>
-          <p>Products: {market.products} </p>
-          <p>Schedule: {market.schedule.replace(/\<br\>/g," ")} </p>
-          <a href= {`${market.GoogleLink}`}> GoogleMaps </a>
+          <img src= "https://cdn.clipart.email/2e03433c29d2fe71d995c36c315481dd_10956-farmers-market-stock-vector-illustration-and-royalty-free-_450-450.jpeg"/>
+          <h1>{market.name.replace(/\d*/g,'')}</h1>
+          <h2>Address: {market.address}</h2>
+          <h2>Products: {market.products} </h2>
+          <h2>Schedule: {market.schedule.replace(/\<br\>/g," ")} </h2>
+          <h3><a href= {`${market.GoogleLink}`}> GoogleMaps </a> </h3>
         </div>
       ))
 
