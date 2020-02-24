8a8d9bf38d6976108411734f98984046753704f4
filmscore88 <cmtmorales24@gmail.com>
2020-02-23 19:36:58 -0600

    refactoring api data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,39 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    };
+  }
 
+  componentDidMount() {
+    fetch(
+      "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=60133"
+    )
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json
+        });
+      });
+  }
 
-    render(){
-
-      return(
+  render() {
+    const { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return;
+      <div>Loading...</div>;
+    } else {
+      return (
         <div className="Homepage">
           <h1>Welcome</h1>
-          <img src= "https://www.shelmerdine.com/wp-content/uploads/2018/05/Farmers-Market.jpg" />
+          <img src="https://www.shelmerdine.com/wp-content/uploads/2018/05/Farmers-Market.jpg" />
         </div>
-
-      )
-
+      );
     }
-
+  }
 }
