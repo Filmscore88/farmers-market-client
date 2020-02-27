be166c41c995dee11401bfc326c70fa979bbb005
filmscore88 <cmtmorales24@gmail.com>
2020-02-27 12:25:41 -0600

    fixed state render

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,8 @@ export default class Home extends Component {
       inputSubmitted: false,
       areaCode: 0
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
   }
 
   handleChange(event) {
@@ -16,15 +18,14 @@ export default class Home extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-
     fetch(
       "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" +
-        `"${areaCode}"`
+        `"${this.state.areaCode}"`
     )
       .then(res => res.json())
       .then(json => {
         this.setState({
-          isSubmitted: true,
+          inputSubmitted: true,
           items: json["results"]
         });
       });
@@ -32,15 +33,17 @@ export default class Home extends Component {
 
   render() {
     const { inputSubmitted, items, areaCode } = this.state;
+    console.log({ inputSubmitted });
     if (!inputSubmitted) {
       return (
-        <form onSubmit ={this.handleOnSubmit}/>
+        <form onSubmit={this.handleOnSubmit}>
           <label>
             Area Code:
-          <input type="number" value={this.state.areaCode} onChange={this.handleChange} />
+            <input type="number" onChange={this.handleChange} />
           </label>
-          <input type="submit" value="Submit"/>
-          </form>
+          <input type="submit" value="Submit" />
+          <img src="https://www.shelmerdine.com/wp-content/uploads/2018/05/Farmers-Market.jpg" />
+        </form>
       );
     } else {
       return (
@@ -50,7 +53,6 @@ export default class Home extends Component {
               <li key={item.id}>{item.marketname}</li>
             ))}
           </ul>
-          <img src="https://www.shelmerdine.com/wp-content/uploads/2018/05/Farmers-Market.jpg" />
         </div>
       );
     }
